ae6f937bab7ff83d208d31805dd97429a57a54cc
Nilesh Patil <128893479+nileshpatil6@users.noreply.github.com>
2025-04-03 18:39:19 +0530

    Update google-maps.ts

--- client/src/lib/google-maps.ts
+++ client/src/lib/google-maps.ts
@@ -7,7 +7,7 @@ declare global {
 export const initGoogleMaps = () => {
   if (!window.google) {
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAp2Dql02fLi7sbKKa6aa-e1ZpGgrLNFfs&libraries=places`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=$API&libraries=places`;
     script.async = true;
     script.defer = true;
     document.head.appendChild(script);
